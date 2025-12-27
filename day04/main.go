48070c7321ecfaaa6c3e60027f03f385f911e327
Janne Lönn <janne@steep.app>
2025-12-27 20:39:10 +0100

    Day 4 solve part 2

--- day04/main.go
+++ day04/main.go
@@ -55,6 +55,69 @@ func part1(grid []string) int {
 	return countAccessible(grid)
 }
 
+func part2(grid []string) int {
+	rows := len(grid)
+	if rows == 0 {
+		return 0
+	}
+	cols := len(grid[0])
+
+	// Create a mutable 2D grid
+	mutableGrid := make([][]byte, rows)
+	for i := range rows {
+		mutableGrid[i] = []byte(grid[i])
+	}
+
+	totalRemoved := 0
+
+	// Directions for 8 adjacent cells
+	dirs := [][]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	// Keep removing until no more accessible rolls
+	for {
+		// Find all accessible rolls
+		accessible := [][2]int{}
+
+		for i := range rows {
+			for j := range cols {
+				if mutableGrid[i][j] == '@' {
+					// Count adjacent paper rolls
+					adjacent := 0
+					for _, dir := range dirs {
+						ni, nj := i+dir[0], j+dir[1]
+						if ni >= 0 && ni < rows && nj >= 0 && nj < cols {
+							if mutableGrid[ni][nj] == '@' {
+								adjacent++
+							}
+						}
+					}
+
+					if adjacent < 4 {
+						accessible = append(accessible, [2]int{i, j})
+					}
+				}
+			}
+		}
+
+		// If no accessible rolls, stop
+		if len(accessible) == 0 {
+			break
+		}
+
+		// Remove all accessible rolls
+		for _, pos := range accessible {
+			mutableGrid[pos[0]][pos[1]] = '.'
+			totalRemoved++
+		}
+	}
+
+	return totalRemoved
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -78,4 +141,5 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(grid))
+	fmt.Printf("Part 2: %d\n", part2(grid))
 }
